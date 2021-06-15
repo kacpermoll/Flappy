bccbde3796cc547771bda735cbeff7668945bc35
kubixondestroyer <72559236+kubixondestroyer@users.noreply.github.com>
2021-06-16 01:37:57 +0200

    Update Bird.cpp

--- Bird.cpp
+++ Bird.cpp
@@ -4,7 +4,14 @@ namespace ChroMoZub
 {
     Bird::Bird(GameDataRef data) : _data(data)
     {
-        this->_birdSprite.setTexture(this->_data->assets.GetTexture("Bird Frame 1"));
+        _animationInterator = 0;
+        _animationFrames.push_back(_data->assets.GetTexture("Bird Frame 1") );
+        _animationFrames.push_back(_data->assets.GetTexture("Bird Frame 2") );
+        _animationFrames.push_back(_data->assets.GetTexture("Bird Frame 3") );
+        _animationFrames.push_back(_data->assets.GetTexture("Bird Frame 4") );
+        
+        this->_birdSprite.setTexture( _animationsFrames.at(_animationInterator));
+        
     }
 
     Bird::~Bird() {
@@ -15,6 +22,25 @@ namespace ChroMoZub
     {
         this->_data->window.draw(_birdSprite);
 
+    }
+    void Bird::Animate(float dt)
+    {
+     if (_clock.getElapsedTime().asSeconds( )>BIRD_ANIMATION_DURATION / _animationFrames.size() )    
+     {
+      if ( _animationInterator < _animationFrames.size( )-1)
+      {
+          _animationInterator++;
+      }
+      else
+       {
+             _animationIterator = 0;
+       }
+        _birdSprite.setTexture(_animationFrames.at(_animationInterator));
+         
+         _clock.restart( );
+         
+         
+     }
     }
   
   
