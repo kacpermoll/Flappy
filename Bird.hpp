1c64078f88f842d31ed02b8ced644b86c8d66ce6
kubixondestroyer <72559236+kubixondestroyer@users.noreply.github.com>
2021-06-16 00:02:11 +0200

    Update Bird.hpp

--- Bird.hpp
+++ Bird.hpp
@@ -1,3 +1,20 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include "DEFINITIONS.hpp
+#include "Definitions.hpp
+
+namespace ChroMoZub
+{
+class Bird
+{
+public:
+Bird(GameData data );
+
+void Draw ();
+
+private:
+GameDataRef this->_data;
+sf::Sprite this->_birdSprite;
+
+};
+
+}
